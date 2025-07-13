e4a5fa6783b1b67eaf20b3ed80de9081beae8ffe
mikee <mikeeckel@hotmail.com>
2025-07-13 17:13:38 -0500

    Initial commit. Blazor app not working as expected

---
+++ src/MVC.Framework.Web/Controllers/AuthorizationController.cs
@@ -0,0 +1,47 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+
+namespace MVC.Framework.Web.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        private IAuthenticationManager AuthenticationManager => HttpContext.GetOwinContext().Authentication;
+
+
+        [AllowAnonymous]
+        public void SignIn()
+        {
+            if (!Request.IsAuthenticated)
+            {
+                // This will trigger a redirect to the external identity provider.
+                // The RedirectUri specifies where to go *after* a successful login.
+                HttpContext.GetOwinContext().Authentication.Challenge(
+                    new AuthenticationProperties {RedirectUri = "/"},
+                    OpenIdConnectAuthenticationDefaults.AuthenticationType);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public void SignOut()
+        {
+            // To sign out, issue a sign-out challenge to both cookie and OIDC middleware.
+            HttpContext.GetOwinContext().Authentication.SignOut(
+                new AuthenticationProperties {RedirectUri = Url.Action("Index", "Home")},
+                OpenIdConnectAuthenticationDefaults.AuthenticationType,
+                CookieAuthenticationDefaults.AuthenticationType);
+        }
+
+        // This action is no longer needed with the corrected SignOut method.
+        // public ActionResult SignOutCallback() { ... }
+
+        [AllowAnonymous]
+        public ActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/MVC.Framework.Web/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Web.Mvc;
+
+namespace MVC.Framework.Web.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        // GET
+        public ActionResult UserInfo()
+        {
+            var identity = User.Identity as ClaimsIdentity;
+
+            if (identity == null)
+            {
+                return new HttpUnauthorizedResult();
+            }
+
+            // Get all claims for the current user
+            var claims = identity.Claims.Select(c => new { Type = c.Type, Value = c.Value }).ToList();
+
+            ViewBag.Claims = claims;
+            ViewBag.Name = identity.FindFirst(ClaimTypes.Name)?.Value ?? "Unknown";
+            ViewBag.Email = identity.FindFirst(ClaimTypes.Email)?.Value;
+            ViewBag.Roles = identity.FindAll(ClaimTypes.Role).Select(c => c.Value).ToList();
+
+            return View();
+        }
+    }
+}
